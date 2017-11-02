e8c9b8aeecf5d4eb001b4885b674ca95a7ecc1de
Jose Narvaez <goyox86@gmail.com>
2017-11-02 22:25:39 +0000

    passwd

--- src/bin/passwd.rs
+++ src/bin/passwd.rs
@@ -1,158 +1,134 @@
-// #![deny(warnings)]
+#![deny(warnings)]
 
-// extern crate arg_parser;
-// extern crate extra;
-// extern crate rand;
-// extern crate termion;
-// extern crate userutils;
+extern crate arg_parser;
+extern crate extra;
+extern crate rand;
+extern crate termion;
+extern crate redox_users;
 
-// use rand::{Rng, OsRng};
-// use std::{env, io};
-// use std::fs::File;
-// use std::io::{Read, Write};
-// use std::process::exit;
+use rand::{Rng, OsRng};
+use std::{env, io};
+use std::io::Write;
+use std::process::exit;
 
-// use arg_parser::ArgParser;
-// use termion::input::TermRead;
-// use userutils::Passwd;
-// use extra::option::OptionalExt;
-// use extra::io::fail;
+use arg_parser::ArgParser;
+use extra::option::OptionalExt;
+use termion::input::TermRead;
+use redox_users::{User, get_uid, get_user_by_uid, get_user_by_name};
 
-// const MAN_PAGE: &'static str = /* @MANSTART{passwd} */ r#"
-// NAME
-//     passwd - modify a user's password
+const MAN_PAGE: &'static str = /* @MANSTART{passwd} */ r#"
+NAME
+    passwd - modify a user's password
 
-// SYNOPSIS
-//     passwd [ user ]
-//     passwd [ -h | --help ]
+SYNOPSIS
+    passwd [ user ]
+    passwd [ -h | --help ]
 
-// DESCRIPTION
-//     The passwd utility changes the user's local password. If the user is not
-//     the super-user, passwd first prompts for the current password and will
-//     not continue unless the correct password is entered.
+DESCRIPTION
+    The passwd utility changes the user's local password. If the user is not
+    the super-user, passwd first prompts for the current password and will
+    not continue unless the correct password is entered.
 
-// OPTIONS
+OPTIONS
 
-//     -h
-//     --help
-//         Display this help and exit.
+    -h
+    --help
+        Display this help and exit.
 
-// AUTHOR
-//     Written by Jeremy Soller.
-// "#; /* @MANEND */
-
-// const PASSWD_FILE: &'static str = "/etc/passwd";
-
-// fn main() {
-//     let stdin = io::stdin();
-//     let mut stdin = stdin.lock();
-//     let stdout = io::stdout();
-//     let mut stdout = stdout.lock();
-//     let mut stderr = io::stderr();
-
-//     let mut parser = ArgParser::new(1)
-//         .add_flag(&["h", "help"]);
-//     parser.parse(env::args());
-
-//     // Shows the help
-//     if parser.found("help") {
-//         stdout.write_all(MAN_PAGE.as_bytes()).try(&mut stderr);
-//         stdout.flush().try(&mut stderr);
-//         exit(0);
-//     }
-
-//     let uid = userutils::get_uid(&mut stderr) as u32;
-
-//     let mut passwd_string = String::new();
-//     let mut passwd_file = File::open(PASSWD_FILE).try(&mut stderr);
-//     passwd_file.read_to_string(&mut passwd_string).try(&mut stderr);
-//     let passwd_file_entries = match Passwd::parse_file(&passwd_string) {
-//         Ok(entries) => entries,
-//         Err(_) => fail("passwd: error parsing passwd file", &mut stderr)
-//     };
-
-//     let passwd = if parser.args.is_empty() {
-//         let passwd_option = passwd_file_entries.iter()
-//             .find(|passwd| passwd.uid == uid);
-
-//         if let Some(passwd) = passwd_option {
-//             passwd
-//         } else {
-//             fail(&format!("passwd: current user id {} does not exist", uid), &mut stderr);
-//         }
-//     } else {
-//         let user = &parser.args[0];
-//         let passwd_option = passwd_file_entries.iter()
-//             .find(|passwd| passwd.user == *user);
-
-//         if let Some(passwd) = passwd_option {
-//             passwd
-//         } else {
-//             fail(&format!("passwd: user '{}' does not exist", user), &mut stderr);
-//         }
-//     };
-
-//     if passwd.uid == uid || uid == 0 {
-//         let msg = format!("changing password for '{}' \n", passwd.user);
-//         stdout.write_all(&msg.as_bytes()).try(&mut stderr);
-//         stdout.flush().try(&mut stderr);
-
-//         let mut verified = false;
-//         if passwd.hash == "" {
-//             verified = true;
-//         } else if passwd.uid == uid || uid != 0 {
-//             stdout.write_all(b"current password: ").try(&mut stderr);
-//             stdout.flush().try(&mut stderr);
-
-//             if let Some(password) = stdin.read_passwd(&mut stdout).try(&mut stderr) {
-//                 stdout.write(b"\n").try(&mut stderr);
-//                 stdout.flush().try(&mut stderr);
-
-//                 if passwd.verify(&password) {
-//                     verified = true;
-//                 }
-//             }
-//         } else {
-//             verified = true;
-//         }
-
-//         if verified {
-//             stdout.write_all(b"new password: ").try(&mut stderr);;
-//             stdout.flush().try(&mut stderr);;
-
-//             if let Some(new_password) = stdin.read_passwd(&mut stdout).try(&mut stderr) {
-//                 stdout.write(b"\nconfirm password: ").try(&mut stderr);
-//                 stdout.flush().try(&mut stderr);
-
-//                 if let Some(confirm_password) = stdin.read_passwd(&mut stdout).try(&mut stderr) {
-//                     stdout.write(b"\n").try(&mut stderr);
-//                     stdout.flush().try(&mut stderr);;
-
-//                     if new_password == confirm_password {
-//                         let salt = format!("{:X}", OsRng::new().try(&mut stderr).next_u64());
-//                         let encoded_passwd = userutils::Passwd::encode(&new_password, &salt);
-//                         //TODO: Actually persist the new password to PASSWD_FILE
-//                         let msg = format!("{}\n", encoded_passwd);
-//                         stdout.write_all(&msg.as_bytes()).try(&mut stderr);
-//                         stdout.flush().try(&mut stderr);
-//                     } else {
-//                         fail("passwd: new password does not match confirm password", &mut stderr);
-//                     }
-//                 } else {
-//                     fail("passwd: no confirm password provided", &mut stderr);
-//                 }
-//             } else {
-//                 fail("passwd: no new password provided", &mut stderr);
-//             }
-//         } else {
-//             fail("passwd: incorrect current password", &mut stderr);
-//         }
-//     } else {
-//         let msg = &format!("passwd: you do not have permission to set the password of '{}'", passwd.user);
-//         fail(msg, &mut stderr);
-//     }
-// }
+AUTHOR
+    Written by Jeremy Soller.
+"#; /* @MANEND */
 
 fn main() {
+    let stdin = io::stdin();
+    let mut stdin = stdin.lock();
+    let stdout = io::stdout();
+    let mut stdout = stdout.lock();
+    let mut stderr = io::stderr();
+
+    let mut parser = ArgParser::new(1)
+        .add_flag(&["h", "help"]);
+    parser.parse(env::args());
+
+    // Shows the help
+    if parser.found("help") {
+        stdout.write_all(MAN_PAGE.as_bytes()).try(&mut stderr);
+        stdout.flush().try(&mut stderr);
+        exit(0);
+    }
+
+    let uid = get_uid();
     
+    let user = if parser.args.is_empty() {
+        get_user_by_uid(uid).unwrap_or_else(|| {
+            eprintln!("passwd: current user id {} does not exist", uid);
+            exit(1);
+        })
+    } else {
+        let username = &parser.args[0];
+        get_user_by_name(username).unwrap_or_else(|| {
+            eprintln!("passwd: user '{}' does not exist", username);
+            exit(1);
+        })
+    };
+
+    let uid = uid as u32;
+    if user.uid == uid || uid == 0 {
+        let msg = format!("changing password for '{}' \n", user.user);
+        stdout.write_all(&msg.as_bytes()).try(&mut stderr);
+        stdout.flush().try(&mut stderr);
+
+        let mut verified = false;
+        if user.hash == "" {
+            verified = true;
+        } else if user.uid == uid || uid != 0 {
+            stdout.write_all(b"current password: ").try(&mut stderr);
+            stdout.flush().try(&mut stderr);
+
+            if let Some(password) = stdin.read_passwd(&mut stdout).try(&mut stderr) {
+                stdout.write(b"\n").try(&mut stderr);
+                stdout.flush().try(&mut stderr);
+
+                if user.verify(&password) {
+                    verified = true;
+                }
+            }
+        } else {
+            verified = true;
+        }
+
+        if verified {
+            stdout.write_all(b"new password: ").try(&mut stderr);;
+            stdout.flush().try(&mut stderr);;
+
+            if let Some(new_password) = stdin.read_passwd(&mut stdout).try(&mut stderr) {
+                stdout.write(b"\nconfirm password: ").try(&mut stderr);
+                stdout.flush().try(&mut stderr);
+
+                if let Some(confirm_password) = stdin.read_passwd(&mut stdout).try(&mut stderr) {
+                    stdout.write(b"\n").try(&mut stderr);
+                    stdout.flush().try(&mut stderr);;
+
+                    if new_password == confirm_password {
+                        let salt = format!("{:X}", OsRng::new().try(&mut stderr).next_u64());
+                        let encoded_passwd = User::encode(&new_password, &salt);
+                        // TODO: Actually persist the new password to PASSWD_FILE
+                        let msg = format!("{}\n", encoded_passwd);
+                        stdout.write_all(&msg.as_bytes()).try(&mut stderr);
+                        stdout.flush().try(&mut stderr);
+                    } else {
+                        eprintln!("passwd: new password does not match confirm password");
+                    }
+                } else {
+                    eprintln!("passwd: no confirm password provided");
+                }
+            } else {
+                eprintln!("passwd: no new password provided");
+            }
+        } else {
+            eprintln!("passwd: incorrect current password");
+        }
+    } else {
+        eprintln!("passwd: you do not have permission to set the password of '{}'", user.user);
+    }
 }
